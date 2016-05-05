31f2c622fb262a5bab6bec85c49ae68310f55ded
Varinder Kumar <bathindavarinder@gmail.com>
2016-05-05 21:14:11 +0530

    First Commit with publish and sample

---
+++ SeleniumRecorder.POMSample/SeleniumRecorder.POMSample/POM/GooglePOm.cs
@@ -0,0 +1,57 @@
+﻿namespace TestNamespace
+{
+    using System;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.UI;
+
+    /// <summary>
+    /// POM of GooglePOM
+    /// </summary>
+    public class GooglePOM
+    {
+
+        #region Constructor
+
+        private IWebDriver driver;
+
+        private string title;
+
+        public GooglePOM(IWebDriver driver)
+        {
+
+            this.driver = driver;
+
+            this.title = driver.Title;
+        }
+        #endregion
+
+        #region Properties
+
+        public IWebElement lst_ib
+        {
+            get
+            {
+                return driver.FindElement(By.Id("lst-ib"));
+            }
+        }
+
+        public IWebElement btnK
+        {
+            get
+            {
+                return driver.FindElement(By.Name("btnK"));
+            }
+        }
+
+        public IWebElement btnI
+        {
+            get
+            {
+                return driver.FindElement(By.Name("btnI"));
+            }
+        }
+
+
+        #endregion
+    }
+}
---
+++ SeleniumRecorder.POMSample/SeleniumRecorder.POMSample/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNamespace; 
+
+namespace SeleniumRecorder.POMSample
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            GooglePOM pom = new GooglePOM(driver);
+             
+        }
+    }
+}
